5e22007e8b9fee66b30aca84409bb58a77385cf7
DuolanK <Duolan.Kharitonov@mail.ru>
2024-12-08 16:08:39 +0900

    3rd comma

---
+++ src/js/app.js
@@ -0,0 +1,23 @@
+function addHoverEffect(element) {
+    element.addEventListener('mousemove', (event) => {
+      const rect = element.getBoundingClientRect();
+      const x = event.clientX - rect.left;
+      const y = event.clientY - rect.top;
+  
+      const moveX = (x / rect.width - 0.5) * 10; // Двигаем на ±5%
+      const moveY = (y / rect.height - 0.5) * 10;
+  
+      element.style.transform = `translate(${moveX}px, ${moveY}px)`;
+    });
+  
+    element.addEventListener('mouseleave', () => {
+      element.style.transform = 'translate(0, 0)';
+    });
+  }
+  
+  // Применяем функцию для каждой картинки и word-grid
+  const images = document.querySelectorAll('.image1');
+
+  
+  images.forEach((image) => addHoverEffect(image));
+  addHoverEffect(wordGrid);
